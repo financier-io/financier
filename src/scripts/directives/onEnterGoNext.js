af7c109e2b68d80f9787f2838e8f5685856c8a08
Alexander Harding <aeharding@gmail.com>
2016-02-16 21:42:52 -0600

    tabindex and <enter> to go to next input box

---
+++ src/scripts/directives/onEnterGoNext.js
@@ -0,0 +1,22 @@
+angular.module('financier').directive('onEnterGoNext', () => {
+  function link(scope, element, attrs) {
+    element.on('keypress', e => {
+      if (e.which === 13) {
+        // enter key
+
+        var els = document.querySelectorAll(`[tabindex='${attrs.tabindex}']`);
+
+        for (var i = 0; i < els.length; i++) {
+          if (els[i] === element[0]) {
+            els[i + 1] && els[i + 1].focus();
+          }
+        }
+      }
+    });
+  }
+
+  return {
+    restrict: 'A',
+    link
+  };
+});
