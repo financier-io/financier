d0216b14c33da95168a8c032b7ac83764e0e88f3
Alexander Harding <aeharding@gmail.com>
2016-02-16 18:02:45 -0600

    Add directive to disable ngAnimate on arbitrary elements (and children)

---
+++ src/scripts/directives/disableNgAnimate.js
@@ -0,0 +1,8 @@
+angular.module('financier').directive('disableNgAnimate', function($animate) {
+  return {
+    restrict: 'A',
+    link: function(scope, element) {
+      $animate.enabled(element, false);
+    }
+  };
+});
