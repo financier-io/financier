397b252266a3f32d29b30f67f960d8ced92705c1
Alexander Harding <aeharding@gmail.com>
2016-02-27 21:05:06 -0600

    Create drop instance on click of note
    To improve rendering performance

--- src/scripts/directives/note.js
+++ src/scripts/directives/note.js
@@ -1,54 +1,59 @@
-angular.module('financier').directive('note', ($sce, $templateRequest, $templateCache, $compile) => {
+angular.module('financier').directive('note', ($sce, $templateRequest, $templateCache, $compile, $timeout) => {
 
   function link(scope, element, attrs, ngModelCtrl) {
-    const templateUrl = $sce.getTrustedResourceUrl('/scripts/directives/note.html');
+    element.on('click', () => {
+      const templateUrl = $sce.getTrustedResourceUrl('/scripts/directives/note.html');
 
-    $templateRequest(templateUrl).then(template => {
-      const wrap = angular.element('<div></div>').append(template);
-      const content = $compile(wrap)(scope);
+      $templateRequest(templateUrl).then(template => {
+        const wrap = angular.element('<div></div>').append(template);
+        const content = $compile(wrap)(scope);
 
-      content.on('keypress keydown', e => {
-        if (e.which === 27) {
-          dropInstance.close();
-        }
-      });
-
-      const dropInstance = new Drop({
-        target: element[0],
-        content: content[0],
-        classes: 'drop-theme-arrows-bounce',
-        openOn: 'click',
-        tetherOptions: {
-          targetOffset: '0 -19px',
-          optimizations: {
-            moveElement: true
+        content.on('keypress keydown', e => {
+          if (e.which === 27) {
+            dropInstance.close();
           }
-        }
-      });
+        });
 
-      dropInstance.on('open', () => {
-        scope.myNote = ngModelCtrl.$viewValue;
-        scope.canDelete = ngModelCtrl.$viewValue &&
-                          ngModelCtrl.$viewValue.length;
-        scope.$apply();
+        const dropInstance = new Drop({
+          target: element[0],
+          content: content[0],
+          classes: 'drop-theme-arrows-bounce',
+          openOn: 'click',
+          tetherOptions: {
+            targetOffset: '0 -19px',
+            optimizations: {
+              moveElement: true
+            }
+          }
+        });
 
-        content.find('textarea')[0].focus();
-      });
+        dropInstance.on('open', () => {
+          scope.myNote = ngModelCtrl.$viewValue;
+          scope.canDelete = ngModelCtrl.$viewValue &&
+                            ngModelCtrl.$viewValue.length;
 
-      dropInstance.on('close', () => {
-        scope.myNote = ngModelCtrl.$viewValue;
-      });
+          content.find('textarea')[0].focus();
+        });
 
-      scope.submit = note => {
-        ngModelCtrl.$setViewValue(note);
+        dropInstance.on('close', () => {
+          scope.myNote = ngModelCtrl.$viewValue;
 
-        dropInstance.close();
-      };
+          $timeout(() => {
+            dropInstance.destroy();
+          });
+        });
 
-      scope.$on('$destroy', () => {
-        dropInstance.destroy();
+        scope.submit = note => {
+          ngModelCtrl.$setViewValue(note);
+
+          dropInstance.close();
+        };
+
+
+        dropInstance.open();
       });
     });
+
   }
 
   return {
