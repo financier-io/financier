fbc13085abce48bbffe62a02f85bb08f94638f95
Alexander Harding <aeharding@gmail.com>
2016-08-06 17:10:22 -0500

    Add ngAnnotate for minification in production

--- webpack.config.js
+++ webpack.config.js
@@ -88,7 +88,7 @@ module.exports = function makeWebpackConfig () {
       // Transpile .js files using babel-loader
       // Compiles ES6 and ES7 into ES5 code
       test: /\.js$/,
-      loader: 'babel',
+      loaders: ['ng-annotate', 'babel'],
       exclude: /node_modules/
     }, {
       // CSS LOADER
