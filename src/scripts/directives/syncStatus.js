bf0ec218fff5c7c9a5758887141d20c900e969e0
Alexander Harding <aeharding@gmail.com>
2016-08-19 15:04:14 -0500

    Change sync status wording

--- src/scripts/directives/syncStatus.js
+++ src/scripts/directives/syncStatus.js
@@ -1,10 +1,21 @@
 angular.module('financier').directive('syncStatus', function() {
   return {
     restrict: 'E',
-    scope: true,
-    template: '{{userCtrl.status}}',
+    template: '{{syncCtrl.textStatus}}',
+    bindToController: {
+      status: '='
+    },
+    controllerAs: 'syncCtrl',
     controller: function($scope, $element) {
       $element.addClass('sync-status');
+
+      $scope.$watch(() => this.status, status => {
+        if (status.indexOf('sync') === -1) {
+          this.textStatus = `Sync ${status}`;
+        } else {
+          this.textStatus = status.charAt(0).toUpperCase() + status.slice(1);;
+        }
+      });
       
       $scope.$watch('userCtrl.status', (newStatus, oldStatus) => {
         $element.removeClass(`sync-status--${oldStatus}`);
