eea3fe0abc41f38236b8872a9f6dc6dac35cd0b1
Alexander Harding <aeharding@gmail.com>
2016-09-25 10:58:44 -0500

    Enable up & down on budget textarea, make work with hidden master categories

--- src/scripts/directives/onEnterGoNext.js
+++ src/scripts/directives/onEnterGoNext.js
@@ -1,22 +1,75 @@
 angular.module('financier').directive('onEnterGoNext', () => {
   function link(scope, element, attrs) {
-    element.on('keypress', e => {
-      if (e.which === 13) {
-        // enter key
+    element.on('keydown', e => {
+      if (e.which === 13 || e.which === 40) {
+        // enter key, down key
+        e.preventDefault();
 
         var els = document.querySelectorAll(`[tabindex='${attrs.tabindex}']`);
 
         for (var i = 0; i < els.length; i++) {
           if (els[i] === element[0]) {
-            if (els.length - 1 === i) {
-              choose(els[0]);
-            } else {
-              choose(els[i + 1]);
-            }
+            return findNext(els, i, i);
+          }
+        }
+      } else if (e.which === 38) {
+        e.preventDefault();
+
+        // up key
+        var els = document.querySelectorAll(`[tabindex='${attrs.tabindex}']`);
+
+        for (var i = 0; i < els.length; i++) {
+          if (els[i] === element[0]) {
+            return findPrevious(els, i, i);
           }
         }
       }
     });
+
+    function findNext(els, originalI, nextI) {
+      const newI = (nextI + 1) % els.length;
+
+      if (originalI === newI) {
+        els[originalI].blur();
+
+        return;
+      }
+
+      if (isVisible(els[newI])) {
+        choose(els[newI]);
+      } else {
+        findNext(els, originalI, newI);
+      }
+    }
+
+    function findPrevious(els, originalI, nextI) {
+      const newI = nextI - 1 < 0 ? els.length - 1 : nextI - 1;
+
+      if (originalI === newI) {
+        els[originalI].blur();
+
+        return;
+      }
+
+      if (isVisible(els[newI])) {
+        choose(els[newI]);
+      } else {
+        findPrevious(els, originalI, newI);
+      }
+    }
+
+    function isVisible(el) {
+      /* offsetParent would be null if display 'none' is set.
+       However Chrome, IE and MS Edge returns offsetParent as null for elements
+       with CSS position 'fixed'. So check whether the dimensions are zero.
+
+       This check would be inaccurate if position is 'fixed' AND dimensions were
+       intentionally set to zero. But..it is good enough for most cases.*/
+      if (!el.offsetParent && el.offsetWidth === 0 && el.offsetHeight === 0) {
+          return false;
+      }
+      return true;
+    }
 
     function choose(el) {
       el.focus();
