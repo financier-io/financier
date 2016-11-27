f888feefeb9aa5109f8a0774371d9abd62f4b5fa
Alexander Harding <aeharding@gmail.com>
2016-11-27 14:38:05 -0600

    Strip emojis when sorting

--- src/scripts/controllers/accountCtrl.js
+++ src/scripts/controllers/accountCtrl.js
@@ -93,9 +93,17 @@ angular.module('financier').controller('accountCtrl', function($translate, $time
     }
   }
 
+  function _removeEmojis(str) {
+    if (angular.isString(str)) {
+      return str.replace(/([#0-9]\u20E3)|[\xA9\xAE\u203C\u2047-\u2049\u2122\u2139\u3030\u303D\u3297\u3299][\uFE00-\uFEFF]?|[\u2190-\u21FF][\uFE00-\uFEFF]?|[\u2300-\u23FF][\uFE00-\uFEFF]?|[\u2460-\u24FF][\uFE00-\uFEFF]?|[\u25A0-\u25FF][\uFE00-\uFEFF]?|[\u2600-\u27BF][\uFE00-\uFEFF]?|[\u2900-\u297F][\uFE00-\uFEFF]?|[\u2B00-\u2BF0][\uFE00-\uFEFF]?|(?:\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDEFF])[\uFE00-\uFEFF]?/g, '').trim();
+    }
+
+    return str;
+  }
+
   this.customSorts = {
     account(transaction) {
-      return manager.getAccount(transaction.account).name;
+      return _removeEmojis(manager.getAccount(transaction.account).name);
     },
     date(transaction) {
       // Sort by date and then value
@@ -105,10 +113,10 @@ angular.module('financier').controller('accountCtrl', function($translate, $time
       return +transaction.checkNumber || transaction.checkNumber;
     },
     category(transaction) {
-      return $scope.dbCtrl.getCategoryName(transaction.category, transaction.date) || '';
+      return _removeEmojis($scope.dbCtrl.getCategoryName(transaction.category, transaction.date)) || '';
     },
     payee(transaction) {
-      return (transaction.transfer ? $scope.dbCtrl.getAccountName(transaction.transfer.account) : $scope.dbCtrl.getPayeeName(transaction.payee)) || '';
+      return (transaction.transfer ? _removeEmojis($scope.dbCtrl.getAccountName(transaction.transfer.account)) : _removeEmojis($scope.dbCtrl.getPayeeName(transaction.payee))) || '';
     },
     cleared(transaction) {
       if (transaction.reconciled) {
